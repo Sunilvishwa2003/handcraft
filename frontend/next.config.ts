8aeb70c52e3850b212791d7bd86d6cae5cf4f695
sunilvishwa <sunilvishwa200@gmail.com>
2026-06-01 22:35:47 +0530

    error fix15

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.1.8'],
-  outputFileTracingRoot: process.cwd(),
   turbopack: {
     root: process.cwd(),
   },
